b48660d47942c755a20d68cb58dcf06385c892dc
piyush <piyushagrawal886@gmail.com>
2026-02-02 23:30:57 +0530

    fix(imports): update import statements to use relative paths in main.py, models.py, and router modules

--- Todo/main.py
+++ Todo/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-import models
-from database import engine
-from router import auth, todos, admin, user
+from .models import Base
+from .database import engine
+from .router import auth, todos, admin, user
 
 app = FastAPI()
 app.include_router(auth.router)
@@ -9,6 +9,6 @@ app.include_router(user.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
